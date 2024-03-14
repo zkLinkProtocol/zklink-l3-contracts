7a6e7c6796f6a769b6626a0c9cc556da81280b91
shonentropy3 <qwerdfkkt@163.com>
2024-03-15 02:52:45 +0800

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,42 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
-
-import "@matterlabs/hardhat-zksync-node";
-import "@matterlabs/hardhat-zksync-deploy";
-import "@matterlabs/hardhat-zksync-solc";
-import "@matterlabs/hardhat-zksync-verify";
-import "@nomiclabs/hardhat-waffle"
-import "@matterlabs/hardhat-zksync-chai-matchers";
-require('solidity-coverage')
-const config: HardhatUserConfig = {
-  defaultNetwork: "inMemoryNode",
-  networks: {
-    inMemoryNode: {
-      url: "http://127.0.0.1:8011",
-      ethNetwork: "localhost", // in-memory node doesn't support eth node; removing this line will cause an error
-      zksync: true,
-    },
-    hardhat: {
-      zksync: true,
-    },
-  },
-  zksolc: {
-    version: "latest",
-    settings: {
-      // find all available options in the official documentation
-      // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
-    },
-  },
+require("@nomicfoundation/hardhat-toolbox");
+require('solidity-coverage');
+require('@nomiclabs/hardhat-ethers');
+// require('@openzeppelin/hardhat-upgrades');
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
   solidity: {
     compilers: [
       {
         version: "0.8.17",
       },
       {
-        version: "0.8.23", 
+        version: "0.8.23",
       },
     ],
     // version: "0.8.17",
   },
 };
-
-export default config;
