b57ebb95565c0d8a02b6b8576c73f17e2f48bbd0
zkbenny <benny@zklink.org>
2024-06-19 13:58:32 +0800

    fix cvf-2

--- contracts/interfaces/IERC20MergeToken.sol
+++ contracts/interfaces/IERC20MergeToken.sol
@@ -3,8 +3,10 @@ pragma solidity 0.8.23;
 
 interface IERC20MergeToken {
     /// @notice Mint merge token
+    /// @dev The caller must be the portal contract
     function mint(address _receiver, uint256 _amount) external;
 
     /// @notice Burn merge token
+    /// @dev The caller must be the portal contract
     function burn(address _from, uint256 _amount) external;
 }
